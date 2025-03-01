3ee6ef39f5704d542248b71af61405d74742932e
Toni Miñarro <antonio.minarro@pccomponentes.com>
2025-03-01 11:51:34 +0100

    Feat: Reorganize routing

--- src/IberdrolaApi/ChargePoint/Entrypoint/Controller/ChargePointController.php
+++ src/IberdrolaApi/ChargePoint/Entrypoint/Controller/ChargePointController.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace MyPlugPlanner\IberdrolaApi\ChargePoint\Entrypoint\Controller;
 
-use MyPlugPlanner\IberdrolaApi\ChargePoint\Application\GetInfo\GetInfoQuery;
-use MyPlugPlanner\IberdrolaApi\ChargePoint\Entrypoint\Requests\ChargePoint\GetInfoRequest;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\MessageBusInterface;
 use PcComponentes\Ddd\Domain\Model\ValueObject\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\HandleTrait;
-use Symfony\Component\Messenger\MessageBusInterface;
+use MyPlugPlanner\IberdrolaApi\ChargePoint\Application\GetInfo\GetInfoQuery;
+use MyPlugPlanner\IberdrolaApi\ChargePoint\Entrypoint\Requests\ChargePoint\GetInfoRequest;
 
+#[Route('/api/v1/charge-point')]
 class ChargePointController extends AbstractController
 {
     use HandleTrait;
@@ -22,6 +24,7 @@ class ChargePointController extends AbstractController
         $this->messageBus = $executeQueryMessage;
     }
 
+    #[Route('/{chargePointId}', methods: ['GET'])]
     public function getInfo(GetInfoRequest $request): JsonResponse
     {
         $query = GetInfoQuery::fromPayload(Uuid::v4(), $request->getPayload());
