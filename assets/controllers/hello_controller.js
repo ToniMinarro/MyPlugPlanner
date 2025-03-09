487446bf090970e62d8e906db737d9c710090465
Toni Miñarro <antonio_jose91@hotmail.es>
2025-03-09 01:13:10 +0100

    Feat: Test Stimulus

--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -13,4 +13,8 @@ export default class extends Controller {
     connect() {
         this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
     }
+
+    greet() {
+        alert('Hello!');
+    }
 }
